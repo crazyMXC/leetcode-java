a28f30bfe0c28f667359ad5ce82069bd79b9322b
crazyMXC <tmxc@bupt.edu.cn>
2018-07-14 23:09:43 +0800

    E12

---
+++ Java/E12-63-IslandPerimeter.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1)
+                    count+=4;
+                if(grid[i][j] == 1 && i - 1 >= 0 && grid[i - 1][j] == 1){
+                    count -= 2;
+                }
+                if(grid[i][j] == 1 && j - 1 >= 0 && grid[i][j - 1] == 1){
+                    count -= 2;
+                }
+            }
+        }
+        return count;
+    }
+}
