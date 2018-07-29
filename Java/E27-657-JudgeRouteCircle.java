b51135760b4e803c343d44d757a6487269bcadb1
crazyMXC <tmxc@bupt.edu.cn>
2018-07-29 23:15:46 +0800

    E27

---
+++ Java/E27-657-JudgeRouteCircle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (char move: moves.toCharArray()) {
+            if (move == 'U') y--;
+            else if (move == 'D') y++;
+            else if (move == 'L') x--;
+            else if (move == 'R') x++;
+        }
+        if(x == 0 && y == 0)
+            return true;
+        else 
+            return false;
+    }
+}
