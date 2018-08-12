d68dacc25554a8ff3f5f20ed6e76aed747e863a4
crazyMXC <tmxc@bupt.edu.cn>
2018-08-12 11:04:49 +0800

    E41

---
+++ Java/E41-459-RepeatedSubstringPattern.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int l=s.length();
+        for(int i=l/2;i>=1;i--){
+            if(l%i==0){
+                int m=l/i;
+                String str=s.substring(0,i);
+                StringBuilder sb= new StringBuilder();
+                for(int j=0;j<m;j++)
+                    sb.append(str);
+                if(sb.toString().equals(s))
+                    return true;
+            }
+        }
+        return false;
+    }
+}
